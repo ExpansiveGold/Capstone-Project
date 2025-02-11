6edabf327c848be4a3de61387e45f1bea620ef68
ExpansiveGold <gabriel.tarumoto@gmail.com>
2025-02-11 15:04:58 -0800

    Update mounted routes

--- server/App.js
+++ server/App.js
@@ -1,19 +1,13 @@
 import express from 'express';
 import dbconnect from './server.js'
-const app = express();
-
-// require("./routes/startup.js")
 import startup from "./routes/startup.js"
-
-startup(app)
-
-// app.use(express.json()); 
 
 // Connect to database
 dbconnect()
+
+// Start app
+const app = express();
+startup(app)
+
 // Starts server
 app.listen(5000, () => console.log('Server started on port 5000'))
-
-app.get('/users', (req, res) => {
-    res.json({'users': ['user1', 'user2', 'user3']})
-})
--- server/routes/startup.js
+++ server/routes/startup.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import create_account from './create_account.js'
+import match from './match.js'
+import friends from './friend_list.js'
+import users from './user_list.js'
 
 function startup(app){
     app.use(express.json());
 
     app.use('/create_account', create_account);
+    
+    app.use('/match', match)
+
+    app.use('/user', friends) // /user/:id/friends
+
+    app.use('/admin/users', users);
+
 };
 
 export default startup
