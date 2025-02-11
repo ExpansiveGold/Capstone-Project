ffd7cd795a9d13b2c6d0028650cd12c3ab3db215
ExpansiveGold <gabriel.tarumoto@gmail.com>
2025-02-11 15:30:27 -0800

    Create puzzles route

---
+++ server/routes/puzzles.js
@@ -0,0 +1,37 @@
+import express from 'express';
+const router = express.Router()
+
+// +---------------+
+// | Puzzels Route |
+// +---------------+
+
+// TODO: Integrate with database
+// /puzzles
+router.route("/") 
+    .get((req, res) => {
+        res.send('Puzzles')
+    })
+    
+// /admin/puzzles
+router.route("/") 
+    .get((req, res) => {
+        res.send('Get all puzzles')
+    })
+
+router.route('/add')
+    .post((req, res) => {
+        res.send('Puzzle added')
+    })
+
+router.route('/edit/:id')
+    .get((req, res) => {
+        res.send('Get puzzle to edit')
+    })
+    .post((req, res) => {
+        res.send('Puzzle edited')
+    })
+
+router.route('/delete/:id')
+    .delete((req, res) => {
+        res.send('Puzzle deleted')
+    })
