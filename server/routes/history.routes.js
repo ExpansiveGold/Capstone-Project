8a1a5ca4dd129b8ab209ed3e2350c4ab580b6205
ExpansiveGold <gabriel.tarumoto@gmail.com>
2025-02-25 19:35:16 -0800

    GET history route implemented

--- server/routes/history.routes.js
+++ server/routes/history.routes.js
@@ -1,54 +1,54 @@
 import express from 'express';
 import db from '../server.js'
-import ObjectId from 'mongodb';
-// import Match from '../models/matches.model.js';
-const DB = db()
+import { ObjectId } from 'mongodb';
 
 const router = express.Router()
-const MatchColl = db().collection("Matches")
+const FullColl = db().collection('fullMatch')
 
-
-// var view = MatchColl.aggregate([
-// var view = db().createCollection('fullMatch', {
-db().createCollection('fullMatch', {
-    viewOn: 'Matches',
-    pipeline: [
-        {
-            $lookup:
-            {
-                from: 'Users',
-                localField: 'white',
-                foreignField: '_id',
-                as: "whitePlayer"
-            },
-        },
-        {  
-            $lookup:
-            {
-                from: 'Users',
-                localField: 'black',
-                foreignField: '_id',
-                as: "blackPlayer"
-            }
-        },
-        {
-            $project:
-            {
-                _id: 0,
-                white: '$whitePlayer.username',
-                black: '$blackPlayer.username',
-                result: 1,
-                duration: 1
-            }
-        },
-        {
-            $unwind: "$white"
-        },
-        {
-            $unwind: "$black"
-        }
-    ]
-})
+// db().createCollection('fullMatch', {
+//     viewOn: 'Matches',
+//     pipeline: [
+//         {
+//             '$lookup': {
+//                 'from': 'Users', 
+//                 'localField': 'white', 
+//                 'foreignField': '_id', 
+//                 'as': 'whitePlayer'
+//             }
+//         }, 
+//         {
+//             '$lookup': {
+//                 'from': 'Users', 
+//                 'localField': 'black', 
+//                 'foreignField': '_id', 
+//                 'as': 'blackPlayer'
+//             }
+//         }, 
+//         {
+//             '$unwind': {
+//                 'path': '$whitePlayer'
+//             }
+//         }, 
+//         {
+//             '$unwind': {
+//                 'path': '$blackPlayer'
+//             }
+//         }, 
+//         {
+//             '$project': {
+//                 '_id': 1, 
+//                 'whitePlayer': '$whitePlayer.username', 
+//                 'blackPlayer': '$blackPlayer.username', 
+//                 'white': 1,
+//                 'black': 1,
+//                 'moves': 1, 
+//                 'movesPgn': 1, 
+//                 'result': 1,
+//                 'duration': 1
+//             }
+//         }
+//     ]
+// })
 
 // +--------------------+
 // | User History Route |
@@ -56,12 +56,11 @@ db().createCollection('fullMatch', {
 
 router.route("/:id/history") // /user/:id/history
 .get(async (req, res) => {
-    console.log(db().collection('fullMatch').find({}))
     const id = req.params['id']
-    var query = { $or: [{ white: id }, { black: id } ]}
+    var query = { $or: [{ white: new ObjectId(id) }, { black: new ObjectId(id) }]}
 
     //Fetch data from database
-    var matches = await MatchColl.find(query).toArray((err, res) => {
+    var matches = await FullColl.find(query).toArray((err, res) => {
         if (err) throw err;
         console.log(res)
         db.close()
